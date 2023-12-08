cf3760ac3adbee54a26fe0b9f0bb1382f14bf643
bramba2000 <bramba2000@gmail.com>
2023-12-08 11:51:06 +0100

    setup flutter project

---
+++ crowdj_app/README.md
@@ -0,0 +1,3 @@
+# crowdj
+
+A new Flutter project for implementing a multi-platform app for dj to enhance their interaction with the crowd.
---
+++ crowdj_app/android/app/src/main/kotlin/it/polimi/dima/crowdj/MainActivity.kt
@@ -0,0 +1,6 @@
+package it.polimi.dima.crowdj
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
