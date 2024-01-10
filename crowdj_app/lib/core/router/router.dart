2eab0a48e76d1d9ee9afafa5c9cebd0b3d9b854d
bramba2000 <bramba2000@gmail.com>
2024-01-10 14:53:12 +0100

    refactor auth page state

--- crowdj_app/lib/core/router/router.dart
+++ crowdj_app/lib/core/router/router.dart
@@ -1,6 +1,6 @@
 import '../../feature/auth/data/auth_data_source.dart';
 import '../../feature/auth/data/user_data_source.dart';
-import '../../pages/signin_page.dart';
+import '../../feature/auth/pages/signin_page.dart';
 import '../../pages/app/EventPage.dart';
 import '../../pages/app/djPages/CreateNewEventPage.dart';
 import '../../feature/auth/providers/authentication_provider.dart';
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 
 import 'package:go_router/go_router.dart';
 import '../../feature/auth/providers/state/authentication_state.dart';
-import '../../pages/login_page.dart';
+import '../../feature/auth/pages/login_page.dart';
 
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
--- crowdj_app/lib/feature/auth/pages/signin_page.dart
+++ crowdj_app/lib/feature/auth/pages/signin_page.dart
@@ -1,12 +1,12 @@
-import '../feature/auth/data/auth_data_source.dart';
-import '../feature/auth/data/user_data_source.dart';
-import '../feature/auth/providers/authentication_provider.dart';
-import '../feature/auth/providers/state/authentication_state.dart';
+import '../data/auth_data_source.dart';
+import '../data/user_data_source.dart';
+import '../providers/authentication_provider.dart';
+import '../providers/state/authentication_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-import '../feature/auth/models/user_props.dart';
+import '../models/user_props.dart';
 
 class SigninPage extends ConsumerStatefulWidget {
   const SigninPage({super.key});
@@ -16,7 +16,6 @@ class SigninPage extends ConsumerStatefulWidget {
 }
 
 class _SigninPageState extends ConsumerState<SigninPage> {
-  
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   bool _wannabeDJ = false; // false = participant, true = DJ
