abc2e53d03fa77b33b2bdfb8b08269304d5040b1
bramba2000 <bramba2000@gmail.com>
2024-02-02 18:03:01 +0100

    improve custom map to enable or disable movement of map

--- crowdj_app/lib/feature/mapHandler/widgets/custom_map.dart
+++ crowdj_app/lib/feature/mapHandler/widgets/custom_map.dart
@@ -6,16 +6,27 @@ import 'package:url_launcher/url_launcher.dart';
 
 import '../models/map_data.dart';
 
+/// A custom map widget that uses the [FlutterMap] package.
+///
+/// It can be used to display a map centered on a specific location or to display
+/// a map with markers. The map can be controlled using a [MapController].
+/// The markers on the map can be provided using a [MapData] object.
 class CustomMap extends StatefulWidget {
   final LatLng? center;
   final MapData? mapData;
   final MapController? mapController;
-  const CustomMap({super.key, this.center, this.mapData, this.mapController})
+  final bool enableMovement;
+  const CustomMap(
+      {super.key,
+      this.center,
+      this.mapData,
+      this.mapController,
+      this.enableMovement = true})
       : assert(mapData != null || center != null,
             "Either center or mapData must be provided");
 
   const CustomMap.fromMapData(MapData this.mapData,
-      {super.key, this.mapController})
+      {super.key, this.mapController, this.enableMovement = true})
       : center = null;
 
   @override
@@ -27,13 +38,16 @@ class _CustomMapState extends State<CustomMap> {
 
   @override
   Widget build(BuildContext context) {
+    final flags = widget.enableMovement
+        ? InteractiveFlag.all
+        : InteractiveFlag.all & ~InteractiveFlag.drag;
     return FlutterMap(
       mapController: widget.mapController,
       options: MapOptions(
-        initialCenter:
-            widget.center ?? widget.mapData!.center ?? const LatLng(0, 0),
-        initialZoom: _initialZoom,
-      ),
+          initialCenter:
+              widget.center ?? widget.mapData!.center ?? const LatLng(0, 0),
+          initialZoom: _initialZoom,
+          interactionOptions: InteractionOptions(flags: flags)),
       children: [
         TileLayer(
           urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
