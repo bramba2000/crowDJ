4953b91952c0d5fcfd83ee75a4f70308195cf00d
bramba2000 <bramba2000@gmail.com>
2024-02-03 13:01:48 +0100

    add provider for current events

---
+++ crowdj_app/lib/feature/mapHandler/providers/current_event.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'current_event.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$currentEventsHash() => r'7a9922753a6d428cb4af07b9aef51cddd567a42d';
+
+/// See also [CurrentEvents].
+@ProviderFor(CurrentEvents)
+final currentEventsProvider =
+    AutoDisposeNotifierProvider<CurrentEvents, List<Event>>.internal(
+  CurrentEvents.new,
+  name: r'currentEventsProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$currentEventsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$CurrentEvents = AutoDisposeNotifier<List<Event>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
