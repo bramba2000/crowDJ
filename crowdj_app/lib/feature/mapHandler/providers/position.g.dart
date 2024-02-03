033a6dbd676d478fe61a647662f7d77ac7c140a1
bramba2000 <bramba2000@gmail.com>
2024-02-03 13:01:10 +0100

    add provider for current position

--- crowdj_app/lib/feature/mapHandler/providers/position.g.dart
+++ crowdj_app/lib/feature/mapHandler/providers/position.g.dart
@@ -6,13 +6,13 @@ part of 'position.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$currentPositionHash() => r'c17b7742ef96b8dbbfe244abfbb02299804ec509';
+String _$currentPositionHash() => r'f8be189612001addebeaf17bffccc172b81e7d03';
 
-/// See also [_currentPosition].
-@ProviderFor(_currentPosition)
-final _currentPositionProvider = AutoDisposeFutureProvider<LatLng>.internal(
-  _currentPosition,
-  name: r'_currentPositionProvider',
+/// See also [currentPosition].
+@ProviderFor(currentPosition)
+final currentPositionProvider = AutoDisposeFutureProvider<LatLng>.internal(
+  currentPosition,
+  name: r'currentPositionProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
       ? null
       : _$currentPositionHash,
@@ -20,6 +20,6 @@ final _currentPositionProvider = AutoDisposeFutureProvider<LatLng>.internal(
   allTransitiveDependencies: null,
 );
 
-typedef _CurrentPositionRef = AutoDisposeFutureProviderRef<LatLng>;
+typedef CurrentPositionRef = AutoDisposeFutureProviderRef<LatLng>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
